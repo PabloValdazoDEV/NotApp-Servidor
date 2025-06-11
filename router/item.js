934660f6f437535117e14a6aa4cb458893c68d15
PabloValdazoDEV <157512231+PabloValdazoDEV@users.noreply.github.com>
2025-06-11 21:39:12 +0200

    feat: logica de crear y editar productos

--- router/item.js
+++ router/item.js
@@ -51,21 +51,76 @@ router.post(
   }
 );
 
-// router.get("/home/:hogar-id", authMiddleware, async (req, res) => {
-//   const { hogar_id } = req.params;
-//   try {
-//     if (!hogar_id) {
-//       return res.status(400).json({ message: "Faltan datos" });
-//     }
-//     const hogar = await prisma.home.findUnique({ where: { id: hogar_id } });
+router.post(
+  "/:item_id",
+  authMiddleware,
+  upload.single("file"),
+  async (req, res) => {
+    const { name, price, description, categories } = req.body;
+    const { item_id } = req.params;
+    try {
+      if (!item_id || !name) {
+        return res.status(400).json({ message: "Faltan datos" });
+      }
+      const item = await prisma.item.findUnique({ where: { id: item_id } });
 
-//     if (!hogar) {
-//       return res.status(400).json({ message: "El hogar no existe" });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// });
+      if (!item) {
+        return res.status(400).json({ message: "El producto no existe" });
+      }
+
+      const image = [];
+
+      if (req.file?.path) {
+        if (item.image) {
+          cloudinary.uploader.destroy(item.image);
+        }
+        const result = await cloudinary.uploader.upload(req.file.path);
+        image.push(result);
+      }else if(item.image){
+        image.push({public_id: item.image});
+      }
+
+      const cleanedName = name?.trim();
+
+      const data = await prisma.item.update({
+        where: { id: item_id },
+        data: {
+          name: cleanedName,
+          image: image[0]?.public_id ? image[0]?.public_id : null,
+          description,
+          price,
+          categories,
+        },
+      });
+      res.json({ message: "Item actualizado correctamente", item: data });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  }
+);
+
+router.delete("/:item_id", authMiddleware, async (req, res) => {
+  const { item_id } = req.params;
+  try {
+    if (!item_id) {
+      return res.status(400).json({ message: "Faltan datos" });
+    }
+    const item = await prisma.item.findUnique({ where: { id: item_id } });
+
+    if (!item) {
+      return res.status(400).json({ message: "El producto no existe" });
+    }
+
+    if (item.image) {
+      cloudinary.uploader.destroy(item.image);
+    }
+    await prisma.item.delete({ where: { id: item_id } });
+    res.json({ message: "Item borrado correctamentename" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 module.exports = router;
